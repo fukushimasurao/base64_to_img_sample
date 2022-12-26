0c6900de367b7c22ce9548e9bdbc869015930567
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-12-26 16:08:29 +0900

    修正

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php
 
-
 // 関数作成追加
 function getExtension($mime_type)
 {
@@ -13,29 +12,41 @@ function getExtension($mime_type)
     return $extensions[$mime_type];
 }
 
-if ($_POST['base64_image']) {
-    // postされたデータの文頭「data:image/png;base64」を消す
-    // 参考 https://stackoverflow.com/questions/15153776/convert-base64-string-to-an-image-file
-    $data = explode(',', $_POST['base64_image']);
+// もしフォームの送信がおかしければ以下エラー文出して処理を終了させる。
+if ($_POST['base64_image'] === '') {
+    exit('ブラウザバックして、もう一度やり直してください。');
+}
 
-    // base64をデコード
-    $image = base64_decode($data[1]);
+// postされたデータの文頭「data:image/png;base64」を消す
+// 参考 https://stackoverflow.com/questions/15153776/convert-base64-string-to-an-image-file
+$data = explode(',', $_POST['base64_image']);
 
-    // fileのmimeタイプ取得(image/jpegが送られてくるので、多分この処理は不要)
-    $finfo = new finfo(FILEINFO_MIME_TYPE);
-    $mime_type = $finfo->buffer($image);
+// base64をデコード
+$image = base64_decode($data[1]);
 
-    // fileの拡張子取得
-    $extension = getExtension($mime_type);
+// fileのmimeタイプ取得(image/jpegが送られてくるので、多分この処理は不要)
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime_type = $finfo->buffer($image);
 
-    // 画像保存先フォルダ（imgフォルダ）がなければ作成
-    if (!file_exists('img')) {
-        mkdir('img', 0777);
-    }
-    // imgファイルパーミッション変更がうまくいかないので明示的指定
-    chmod('img', 0755);
+// fileの拡張子取得
+$extension = getExtension($mime_type);
 
-    // ファイルの一時保存
-    $file_name = date('YmdHis')."_image.{$extension}";
-    file_put_contents('img/' . $file_name, $image);
+// 画像保存先フォルダ（imgフォルダ）がなければ作成
+if (!file_exists('img')) {
+    mkdir('img', 0777);
+}
+// imgファイルパーミッション変更がうまくいかないので明示的指定
+chmod('img', 0777);
+
+// ファイルの保存(名前は、日付 + _image.jpeg の形にしてます。)
+$file_name = date('YmdHis')."_image.{$extension}";
+file_put_contents('img/' . $file_name, $image);
+
+if (file_exists('img/' . $file_name)) {
+    // 作業しやすい様に、権限変えちゃってます。
+    chmod('img/' . $file_name, 0777);
+    echo 'imgフォルダに保存しました。';
+    echo '保存名 : ' . 'img/' . $file_name;
+} else {
+    echo '保存できませんでした。.';
 }
